0c2b7b47ea3e8a17aa0ff52d69b59c45189a0d75
FPS MATEUS <112101447+mateus327@users.noreply.github.com>
2024-11-05 14:10:35 -0300

    visuaizar

--- graficos/informaçoesGlobais.js
+++ graficos/informaçoesGlobais.js
@@ -1,2 +1,8 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json&#39;
-console.log
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json&#39'
+async function visualizarInformacoesGlobais(params) {
+const res = await fetch (url);
+const dados = await res.json ()
+console.log (dados);
+
+}
+visualizarInformacoesGlobais ()
